ec3ff09558263cda12f5b15d5b99a352c0a71d95
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-11-04 14:13:29 +0100

    enabled git

--- benchmark_record_action/benchmark.py
+++ benchmark_record_action/benchmark.py
@@ -43,7 +43,7 @@ def benchmark(config):
     world_config = config['world']
 
     # Initialise Git
-    #git.init()
+    git.init()
 
     # Parse input competitor
     competitor = _get_competitor()
@@ -54,7 +54,7 @@ def benchmark(config):
     _remove_competitor_controller()
 
     # Commit and Push updates
-    #git.push(message="record and update benchmark animations")
+    git.push(message="record and update benchmark animations")
 
 
 def _get_competitor():
